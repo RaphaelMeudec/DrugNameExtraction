ee7fdfafb1fa5049e9d74337393704a09831bc18
raph <raphael.meudec@gmail.com>
2017-07-07 18:14:01 +0200

    Need to do some distance analysis between words

--- drug_extraction.py
+++ drug_extraction.py
@@ -1,20 +1,32 @@
 import pandas as pd
 import metaphone
+import tqdm
 
 
-def generate_list_of_drugs(paths):
+def load_rxnorm(paths):
     """
     Generate list of drug names based on rxnorm database.
 
     :param paths: dict | Dictionnary of path with table names as keys
     :return: pd.DataFrame | Data
     """
-    rxnconso_df = pd.read_csv(paths['rxnconso'])
-    rxnrel_df = pd.read_csv(paths['rxnrel'])
-    return rxnconso_df, rxnrel_df
+    df_rxnconso = pd.read_csv(paths['rxnconso'])
+    df_rxnconso = df_rxnconso[df_rxnconso['SAB'] == "RXNORM"]
+
+    return df_rxnconso
 
 
-def extract_drug_names(l, names):
+def add_metaphone_to_rxnorm(df_rxnconso):
+    names = df_rxnconso['STR']
+    names_low = [name.lower().strip() for name in names]
+    names_meta = [metaphone.doublemetaphone(name) for name in names_low]
+    df_rxnconso['low'] = names_low
+    df_rxnconso['dmeta'] = names_meta
+    print(df_rxnconso.values.shape)
+    return df_rxnconso
+
+
+def extract_drug_names(l, df_rxnconso):
     """
     Extract drug names from a list of words.
 
@@ -22,14 +34,15 @@ def extract_drug_names(l, names):
     :param names: list | List of possible drug names
     :return: list | List of drug names in the list
     """
-    names_low = [name.lower().strip() for name in names]
-    names_meta = [metaphone.doublemetaphone(name) for name in names]
-    result = []
-    for x in l:
+    extracted_names = []
+    for x in tqdm.tqdm(l, desc="Tokens"):
         x_low = x.lower().strip()
-        if x_low in names_low:
-            result.append((x, "true_eq"))
-        elif metaphone.doublemetaphone(x_low) in names_meta:
-            result.append((x, "meta_eq"))
+        x_meta = metaphone.doublemetaphone(x_low)
+        if x_low in df_rxnconso['low'].unique():
+            extracted_names.append((x_low, "true_eq"))
+        else:
+            for el in df_rxnconso['dmeta'].unique():
+                if (el == x_meta) & (len(el[0]) > 1):
+                    extracted_names.append((x_low, "meta_eq"))
 
-    return result
+    return extracted_names
