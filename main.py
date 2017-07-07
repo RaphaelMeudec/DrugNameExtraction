c6e8e131380b0e4f989471cf92fe38bb062d33e9
raph <raphael.meudec@gmail.com>
2017-07-07 18:12:40 +0200

    Add arguments

--- main.py
+++ main.py
@@ -7,31 +7,37 @@ import drug_extraction as de
 
 @click.command()
 @click.option('--filepath', help="Path to file")
-@click.option('--rxnormpath', help="Path to the rxnorm directory containing the .csv")
-def main(filepath, rxnormpath):
+@click.option('--rxnormpath', default="rxnorm/", help="Path to the rxnorm directory containing the .csv")
+@click.option('--verbose', default=1, help="Verbose")
+def main(filepath, rxnormpath, verbose):
+    if verbose:
+        print("Setting paths to RXNORM.")
     RXNORM_PATH = rxnormpath
     RXNCONSO_PATH = os.path.join(RXNORM_PATH, "rxnconso.csv")
-    RXNREL_PATH = os.path.join(RXNORM_PATH, "rxnrel.csv")
-    RXNSAB_PATH = os.path.join(RXNORM_PATH, "rxnsab.csv")
-    RXNSAT_PATH = os.path.join(RXNORM_PATH, "rxnsat.csv")
-    RXNSTY_PATH = os.path.join(RXNORM_PATH, "rxnsty.csv")
 
+    if verbose:
+        print("Loading file into tokens.")
     tokens = fp.text_file_to_tokens(filepath)
+    if verbose:
+        print("Remove stopwords from tokens.")
     clean_tokens = fp.remove_stopwords(tokens)
 
     rxnorm_paths = {
-        'rxnconso': RXNCONSO_PATH,
-        'rxnrel': RXNREL_PATH,
-        'rxnsab': RXNSAB_PATH,
-        'rxnsat': RXNSAT_PATH,
-        'rxnsty': RXNSTY_PATH
+        'rxnconso': RXNCONSO_PATH
     }
 
-    drugs = ['methadone', 'protonix', 'lovenox', 'azithromycin']
-    # drugs = de.generate_list_of_drugs(rxnorm_paths)
-
-    drug_names = de.extract_drug_names(clean_tokens, drugs)
-    print(drug_names)
+    if verbose:
+        print("Loading RXNORM.")
+    df_rxnconso = de.load_rxnorm(rxnorm_paths)
+    if verbose:
+        print("Adding lower values and metaphone values")
+    df_rxnconso = de.add_metaphone_to_rxnorm(df_rxnconso)
+    if verbose:
+        print("Extracting drug names from file.")
+    drug_names = de.extract_drug_names(clean_tokens, df_rxnconso)
+    if verbose:
+        print("Done.")
+        print(drug_names)
 
 
 if __name__ == "__main__":
