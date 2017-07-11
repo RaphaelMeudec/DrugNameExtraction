dbfbf55cc7be051b297f74b42142136b0cfaf6a2
raph <raphael.meudec@gmail.com>
2017-07-11 16:59:37 +0200

    Hello docstring my old friend

--- drug_extraction.py
+++ drug_extraction.py
@@ -18,6 +18,12 @@ def load_rxnorm(paths):
 
 
 def extract_drug_names_from_rxnorm(df_rxnconso):
+    """
+    Extract list of drug names from the RXNORM Conso pd.DataFrame.
+
+    :param df_rxnconso: pd.DataFrame | Dataframe of RXNORM CONSO table
+    :return: set | List of drug names
+    """
     df_names = df_rxnconso['STR']
     names = df_names.unique()
     # Removing all composed names as we split text on spaces
@@ -27,10 +33,24 @@ def extract_drug_names_from_rxnorm(df_rxnconso):
 
 
 def strict_equal_criteria(word1, word2):
+    """
+    Test if two words are equal.
+
+    :param word1: str | First word
+    :param word2: str | Second word
+    :return: boolean | Are they the same
+    """
     return word1 == word2
 
 
 def levenshtein_distance(word1, word2):
+    """
+    Compute the levenshtein distance between two words.
+
+    :param word1: str | First word
+    :param word2: str | Second word
+    :return: float | Distance
+    """
     size1, size2 = len(word1), len(word2)
     dist = np.zeros((size1+1, size2+1))
     dist[:, 0] = np.arange(size1+1)
@@ -45,6 +65,12 @@ def levenshtein_distance(word1, word2):
 
 
 def levenshtein_criteria(distance, word):
+    """
+    Test if the distance is small enough in comparison to the word length.
+
+    :param distance: float | Distance value
+    :param word: str | Word that has been compared
+    """
     length = len(word)
     if length == 0:
         return 0
@@ -52,6 +78,13 @@ def levenshtein_criteria(distance, word):
 
 
 def metaphone_criteria(word1, word2):
+    """
+    Apply a criteria based on metaphone comparison, and on resemblance between words.
+
+    :param word1: str | First word
+    :param word2: str | Second word
+    :return: boolean | Are they close enough ?
+    """
     word1_meta = metaphone.doublemetaphone(word1)
     word2_meta = metaphone.doublemetaphone(word2)
     metaphone_apply = word1_meta == word2_meta
@@ -65,6 +98,13 @@ def metaphone_criteria(word1, word2):
 
 
 def are_words_close(word1, word2):
+    """
+    Apply strict equality test and then metaphone test to two words.
+
+    :param word1: str | First word
+    :param word2: str | Second word
+    :return: str | Type of relation between words (true_eq, meta_eq, no_eq)
+    """
     are_equal = strict_equal_criteria(word1, word2)
     if are_equal:
         return "true_eq"
@@ -77,6 +117,13 @@ def are_words_close(word1, word2):
 
 
 def is_word_in_list(word, drug_names):
+    """
+    Test if a word is related to words in a list.
+
+    :param word: str | Word to test
+    :param drug_names: list | List of drug names
+    :return: str | If a word of the list is linked, returns the relation type ("true_eq", "meta_eq"), else return "no_eq"
+    """
     word_low = word.lower().strip()
     for el in drug_names:
         relation = are_words_close(word_low, el)
