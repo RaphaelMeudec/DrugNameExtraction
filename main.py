c317f792832e41fd22752577cf1d101809bd7356
raph <raphael.meudec@gmail.com>
2017-07-11 16:40:29 +0200

    extract_drug_names takes list of drug names instead of pd.DataFrame: impacting main.py

--- main.py
+++ main.py
@@ -31,10 +31,10 @@ def main(filepath, rxnormpath, verbose):
     df_rxnconso = de.load_rxnorm(rxnorm_paths)
     if verbose:
         print("Adding lower values and metaphone values")
-    df_rxnconso = de.add_metaphone_to_rxnorm(df_rxnconso)
+    drug_names_list = de.add_metaphone_to_rxnorm(df_rxnconso)
     if verbose:
         print("Extracting drug names from file.")
-    drug_names = de.extract_drug_names(clean_tokens, df_rxnconso)
+    drug_names = de.extract_drug_names(clean_tokens, drug_names_list)
     if verbose:
         print("Done.")
         print(drug_names)
